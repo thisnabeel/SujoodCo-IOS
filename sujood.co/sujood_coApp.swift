6e611d6b43fc9ff236a28e14e6e5dd0649ff2343
funseshon <funseshon@gmail.com>
2026-02-26 16:14:50 -0800

    Initial Commit

---
+++ sujood.co/sujood_coApp.swift
@@ -0,0 +1,17 @@
+//
+//  sujood_coApp.swift
+//  sujood.co
+//
+//  Created by Nabeel Khan on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct sujood_coApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
